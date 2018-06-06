10dce7a730817ddc9a1bcff4e1019e6463d83295
Andrey Koltsov <a99920641@gmail.com>
2018-06-07 00:04:46 +0300

    Legacy pieces of code were removed.

--- src/com/company/RAM_independent_implimentation/CSVexternalFileJoiner.java
+++ src/com/company/RAM_independent_implimentation/CSVexternalFileJoiner.java
@@ -81,7 +81,6 @@ public class CSVexternalFileJoiner {
     }
 
     private static List<CSVFileElement> getElementFromBucket(Path targetFilePath, int startingLine, int amountOfRows) throws IOException {
-        // todo: add illegal argument exception for parameters
         List<CSVFileElement> elementsInBucket = new ArrayList<>();
         try (BufferedReader bufferedReader = Files.newBufferedReader(targetFilePath)) {
             String currentLine = null;
@@ -140,73 +139,5 @@ public class CSVexternalFileJoiner {
             }
         }
         return result;
-    }
-
-
-    // NOTE: Legacy advanced method which is using RAM
-    private static void legacyAdvancedMethod(List<Path> paths) throws IOException {
-        Path resultFilePath = Paths.get("result.csv"); // NOTE: Creating a result file
-        try (BufferedWriter bufferedWriter = Files.newBufferedWriter(resultFilePath)) {
-            File fileA = new File(paths.get(0).toUri());
-            File fileB = new File(paths.get(1).toUri());
-            BufferedReader bufferedReaderForFileA = new BufferedReader(new FileReader(fileA));
-            BufferedReader bufferedReaderForFileB = new BufferedReader(new FileReader(fileB));
-            final int BUCKET_SIZE = 15; // NOTE: ammount of rows that could be read in one iteration
-            int amountOfLinesRead = 0;
-            String currentLineInFileA = null;
-            String currentLineInFileB = null;
-            List<String> dataFromFileA = new ArrayList<>();
-            List<String> dataFromFileB = new ArrayList<>();
-            boolean fileAhasMoreData = true;
-            boolean fileBhasMoreData = true;
-            // TODO: add separate functions for both of the files
-            // ... FILE A should be in a linked-hash-map
-            // ... FILE B - we'd store only values to we ould ocmpare it easily
-            while ((fileAhasMoreData || fileBhasMoreData)) {
-                while (amountOfLinesRead <= BUCKET_SIZE) {
-                    if ((currentLineInFileA = bufferedReaderForFileA.readLine()) != null) {
-                        dataFromFileA.add(currentLineInFileA);
-                    } else {
-                        fileAhasMoreData = false;
-                    }
-                    if ((currentLineInFileB = bufferedReaderForFileB.readLine()) != null) {
-                        dataFromFileB.add(currentLineInFileB);
-                    } else {
-                        fileBhasMoreData = false;
-                    }
-                    amountOfLinesRead++;
-                }
-                Map<String, String> result = putDataIntoMap(dataFromFileA);
-                writeMergedDataOntoFile(result, dataFromFileB, bufferedWriter);
-            }
-
-        } catch (IOException error) {
-            // todo: change error message
-            System.out.println("An error has occured while streaming into the file: " + resultFilePath + error.getMessage());
-            System.exit(Constants.RUNTIME_ERROR_CODE);
-        }
-    }
-
-    private static void writeMergedDataOntoFile(Map<String, String> map, List<String> list, BufferedWriter bufferedWriter) throws IOException {
-        boolean isEndOfLineSymbolNeeded = false; // NOTE: BufferedWrited doesn't provide writing as a line, so we'd add the EOL symbol if needed
-        for (int i = 0; i < list.size(); ++i) {
-            String currentString = list.get(i).toString();
-            // String numberAsKey = currentString.substring(0, Constants.AMOUNT_OF_DIGITS_IN_KEY);
-            String numberAsKey = getKeyFromLine(currentString);
-            if (!map.keySet().contains(numberAsKey)) {
-                continue;
-            }
-            final int amountOfSeparators = 1;
-            String csvStrValueWithSeparator = currentString.substring(Constants.AMOUNT_OF_DIGITS_IN_KEY, Constants.AMOUNT_OF_DIGITS_IN_KEY + Constants.AMOUNT_OF_CHARACTERS_IN_VALUE + 1);
-            String valueForCurrentKey = map.get(numberAsKey);
-            if (!hasAttachedData(valueForCurrentKey)) {
-                String endOfLineSymbol = isEndOfLineSymbolNeeded ? "\n" : "";
-                bufferedWriter.write(endOfLineSymbol + numberAsKey + "," + valueForCurrentKey + csvStrValueWithSeparator);
-                isEndOfLineSymbolNeeded = true;
-            } else {
-                String originalValue = valueForCurrentKey.substring(Constants.AMOUNT_OF_DIGITS_IN_KEY + amountOfSeparators, Constants.AMOUNT_OF_DIGITS_IN_KEY + Constants.AMOUNT_OF_CHARACTERS_IN_VALUE + amountOfSeparators);
-                bufferedWriter.write( numberAsKey + "," + originalValue + csvStrValueWithSeparator);
-            }
-        }
     }
 }
