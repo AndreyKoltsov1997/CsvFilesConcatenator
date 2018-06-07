53dc7752cebe51b215f0f8deff3c8e8c9d99e4fa
Andrey Koltsov <a99920641@gmail.com>
2018-06-07 03:57:12 +0300

    Add files as a source.

--- src/com/company/CsvFilesConcatenator.java
+++ src/com/company/CsvFilesConcatenator.java
@@ -1,28 +1,33 @@
 package com.company;
+import com.company.RAM_dependend_implimentation.CSVFileJoiner;
 import com.company.utilities.Constants;
 
 import java.io.*;
+import java.net.URL;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.*;
 import java.util.List;
 
-import static com.company.utilities.ParsingUtils.*;
-import com.company.RAM_dependend_implimentation.*;
 import com.company.RAM_independent_implimentation.*;
 
 
 public class CsvFilesConcatenator {
 
     public static void main(String[] args) throws IOException {
-        List<Path> paths = Arrays.asList(Paths.get("/Users/Andrey/Desktop/INPUT_A.csv"), Paths.get("/Users/Andrey/Desktop/INPUT_B.csv"));
-       // List<Path> paths = Arrays.asList(Paths.get("/Users/Andrey/Desktop/input_large_file_A.csv"), Paths.get("/Users/Andrey/Desktop/input_large_file_B.csv"));
+        List<Path> paths = Arrays.asList(Paths.get("INPUT_A.csv"), Paths.get("INPUT_B.csv"));
 
         try {
             /** "SIMPLE" implimentation: */
-            //CSVFileJoiner.perform(paths);
+
+            CSVFileJoiner.perform(paths);
+
             /** "ADVANCED" implimentation: */
+            /** I've been testing  "Advanced" implimentation on a files 3 000 000 rows in each. (~80 mb each approx.),
+             * .. it took me around a minute to sort. I won't be pushing the files into Git due to the large size. */
+
             CSVexternalFileJoiner.perform(paths);
+
         } catch (IOException error) {
             System.out.println("An error has occured while reading the data. " + error.getMessage());
             System.exit(Constants.RUNTIME_ERROR_CODE);
