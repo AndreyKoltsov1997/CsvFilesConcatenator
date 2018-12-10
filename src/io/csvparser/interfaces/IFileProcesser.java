7e91fbfd998c448f943521e080b667e4d32d6f4c
Andrey Koltsov <a99920641@gmail.com>
2018-12-10 20:15:12 +0300

    Project clean up in order to remote ignoring files.

--- src/io/csvparser/interfaces/IFileProcesser.java
+++
@@ -1,9 +0,0 @@
-package io.csvparser.interfaces;
-
-import java.io.IOException;
-import java.nio.file.Path;
-import java.util.List;
-
-public interface IFileProcesser {
-     void perform(List<Path> paths) throws IOException;
-}
